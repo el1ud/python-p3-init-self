0484c2136a706d937a68ed430c503faf32846711
eliud wainaina <wainainaeliud7@gmail.com>
2024-06-19 13:29:51 -0700

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+ def __init__(self, name, breed ="Mutt"):
+  self.name = name
+  self.breed = breed
+
+ #def _init_(self,breed):
+  #self.breed = breed
+
+#fido.breed
